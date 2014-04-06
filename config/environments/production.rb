09b7fed5cc625fd5b2f655ec7391af3a0201a3ce
Vineet Gupta <vi_gupta@hotmail.com>
2014-04-06 13:54:35 +0530

    dss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ ALYND::Application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
@@ -27,7 +27,7 @@ ALYND::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
